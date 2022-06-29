763aedf1fbee27aea41f23c6f017ed930dd0358f
k1ra0 <illkiri02@mail.ru>
2022-06-29 17:35:52 +0300

    кира
    столько всякой еботы, ты бы только знал

---
+++ Challenge/gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require ('gulp');
+const concat = require ('gulp-concat');
+const browserSync = require ('browser-sync').create();
+const del = require ('del');
+
+const cssFiles = [
+    './src/css/style.less',
+    './src/css/all.less',
+    './src/css/styled2.less',
+    './src/css/styled3.less'
+]
+
+function styles () {
+    return gulp.src(cssFiles)
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
+}
+
+function clean() {
+    return del(['build/*'])
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('/src/css/**/*.css', styles)
+    gulp.watch("./*.html").on('change', browserSync.reload);
+
+}
+
+gulp.task ('styles', styles);
+gulp.task ('del', clean);
+gulp.task ('watch', watch);
+gulp.task ('build', gulp.series(clean, styles));
+gulp.task ('dev', gulp.series("build", "watch"));
